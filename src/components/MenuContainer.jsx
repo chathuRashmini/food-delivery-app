bf9535d97f1b02edbb2558f4f919de16c0699f9b
chathu_08 <chathu.rash97@gmail.com>
2022-11-26 04:57:27 +0530

    Implemented the menu container

--- src/components/MenuContainer.jsx
+++ src/components/MenuContainer.jsx
@@ -1,11 +1,17 @@
 import React, { useState } from 'react'
-import { IoFastFood } from 'react-icons/io5'
+import { motion  } from 'framer-motion'
+
+import { RowContainer } from '../components'
 
 import { categories } from '../utils/data'
+import { useStateValue } from '../context/StateProvider'
 
 const MenuContainer = () => {
 
     const [filter, setfilter] = useState('chicken')
+
+    const [{foodItems}, dispatch] = useStateValue()
+    
 
     return (
         <section className="w-full my-8 md:my-0" id='menu'>
@@ -14,13 +20,16 @@ const MenuContainer = () => {
                     before:absolute before:rounded-lg before:content before:w-32 before:h-1 before:-bottom-2 before:left-0 before:bg-gradient-to-tr from-orange-200 to-orange-700
                     transition-all ease-in-out duration-100 mr-auto
                 ">
-                    Hot and Yummy Dsihes!
+                    Check what awaits for you
                 </p>
 
                 <div className="w-full flex items-center justify-start lg:justify-center gap-8 py-6 overflow-x-scroll scrollbar-none mt-8">
                     { categories && categories.map((item, index) => (
-                        <div className={`group ${filter === item.urlParamName ? 'bg-green-400' : 'bg-white'} w-24 min-w-[94px] h-28 cursor-pointer rounded-lg drop-shadow-lg flex flex-col 
+                        <motion.div 
+                            whileTap={{ scale: 0.75 }}
+                            className={`group ${filter === item.urlParamName ? 'bg-green-400' : 'bg-white'} w-24 min-w-[94px] h-28 cursor-pointer rounded-lg drop-shadow-lg flex flex-col 
                             hover:bg-red-500 gap-3 items-center justify-center duration-150 transition-all ease-in-out`}
+                            onClick={() => setfilter(item.urlParamName)}
                         >
                             <div key={index} className={`w-10 h-10 rounded-full ${filter === item.urlParamName ? 'bg-card' : 'bg-red-500'} group-hover:bg-card flex items-center justify-center`}>
                                 <span className={` ${filter === item.urlParamName ? 'text-green-400' : 'text-white'} text-lg group-hover:text-red-500`}>
@@ -28,8 +37,12 @@ const MenuContainer = () => {
                                 </span>
                             </div>
                             <p className="text-sm text-textColor group-hover:text-white">{item.name}</p>
-                        </div>
+                        </motion.div>
                     ))}
+                </div>
+
+                <div className="w-full">
+                    <RowContainer data={foodItems?.filter(n => n.category === filter)} flag={false} />
                 </div>
             </div>
         </section>
--- src/components/RowContainer.jsx
+++ src/components/RowContainer.jsx
@@ -2,6 +2,8 @@ import React, { useEffect, useRef } from 'react'
 import { MdShoppingBasket } from 'react-icons/md'
 import { motion } from 'framer-motion'
 
+import NotFound from './img/NotFound.svg'
+
 const RowContainer = ({ data, flag, scrollValue }) => {
 
     const rowContainer = useRef()
@@ -14,39 +16,44 @@ const RowContainer = ({ data, flag, scrollValue }) => {
         <div ref={rowContainer} className={`w-full mt-8 mb-2 flex items-center justify-center gap-3 scroll-smooth
             ${flag ? 'overflow-x-scroll scrollbar-none' : 'overflow-x-hidden flex-wrap'}`}
         >
-            { data && data.map((item, index) => (
-                <div key={index} className="w-auto h-[175px] md:w-300 md:min-w-[300px]  bg-cardOverlay rounded-lg py-2 px-4 my-12 backdrop-blur-lg hover:drop-shadow-lg flex flex-col items-center justify-evenly relative">
-                    <div className="w-full flex items-center justify-between">
-                        <motion.img 
-                            src={item.imageURL}
-                            alt="fruits" 
-                            className="w-20 h-20 lg:w-40 lg:h-40 -mt-8 drop-shadow-2xl" 
-                            whileHover={{ scale: 1.2 }}
-                        />
-
-                        <motion.div 
-                            className="w-6 h-6 lg:w-8 lg:h-8 rounded-full bg-red-500 flex items-center justify-center"
-                            whileTap={{ scale: 0.75 }}
-                        >
-                            <MdShoppingBasket className='text-white' />
-                        </motion.div>
-                    </div>
-
-                    <div className="w-full flex items-end justify-between">
-                        <p className="text-textColor font-semibold text-base md:text-lg">
-                            {item.title}
-                        </p>
-                        {/* <div className="flex flex-col">
-                        <p className="mt-2 text-sm text-gray-500">{item.calories}</p> */}
-                            <div className="flex items-center gap-8">
-                                <p className="text-lg text-textColor font-semibold">
-                                    <span className="text-sm text-red-400">$ </span>{item.price}
-                                </p>
-                            {/* </div> */}
+            { data?.length > 0  ? (
+                data.map((item, index) => (
+                    <div key={index} className="w-auto h-[175px] md:w-300 md:min-w-[300px]  bg-cardOverlay rounded-lg py-2 px-4 my-12 backdrop-blur-lg hover:drop-shadow-lg flex flex-col items-center justify-evenly relative">
+                        <div className="w-full flex items-center justify-between">
+                            <motion.div className="w-20 h-20 lg:w-36 lg:h-36 -mt-8 drop-shadow-2xl"  whileHover={{ scale: 1.2 }}>
+                                <img 
+                                    src={item.imageURL}
+                                    alt={item.title} 
+                                    className="w-full h-full object-contain" 
+                                />
+                            </motion.div>
+    
+                            <motion.div 
+                                className="w-6 h-6 lg:w-8 lg:h-8 rounded-full bg-red-500 flex items-center justify-center"
+                                whileTap={{ scale: 0.75 }}
+                            >
+                                <MdShoppingBasket className='text-white' />
+                            </motion.div>
+                        </div>
+    
+                        <div className="w-full flex items-end justify-between">
+                            <p className="text-textColor font-semibold text-base md:text-lg">
+                                {item.title}
+                            </p>
+                                <div className="flex items-center gap-8">
+                                    <p className="text-lg text-textColor font-semibold">
+                                        <span className="text-sm text-red-400">$ </span>{item.price}
+                                    </p>
+                            </div>
                         </div>
                     </div>
+                ))
+            ) : (
+                <div className="w-full flex flex-col items-center justify-center">
+                    <img src={NotFound} alt='No items' className='h-340' />
+                    <p className="text-xl text-headingColor my-4">Not items available</p>
                 </div>
-            ))}
+            )}
         </div>
     )
 }
